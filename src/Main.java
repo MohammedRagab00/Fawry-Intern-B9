1dd5dae335c1551b4ca18576b7785e27d1a4e961
MohammedRagab00 <123463070+MohammedRagab00@users.noreply.github.com>
2025-07-05 02:37:16 +0300

    last dance

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import cart.entity.Cart;
 import cart.entity.CartItem;
 import cart.service.CartService;
+import cart.service.ICartService;
 import product.ExpirableShippableProduct;
 import product.Product;
 import product.ShippableProduct;
@@ -11,13 +13,18 @@ public class Main {
     public static void main(String[] args) {
         Product cheese = new ExpirableShippableProduct("Cheese", 10.99, 10
                 , LocalDateTime.of(2026, 10, 1, 0, 0, 0), 21, 2);
-        Product TV  = new ShippableProduct("TV", 9999.99, 2, 1000, 10);
+        Product TV = new ShippableProduct("TV", 9999.99, 2, 1000, 10);
 
 
         User user = new User("Mohammed Ragab", "<EMAIL>", "+201556070011", 50000);
         user.getCart().addCartItem(new CartItem(cheese, 4));
         user.getCart().addCartItem(new CartItem(TV, 1));
 
-        new CartService(user.getCart()).checkout();
+        var cartService = getCartService(user.getCart());
+        cartService.checkout();
+    }
+
+    public static ICartService getCartService(Cart cart) {
+        return new CartService(cart);
     }
 }
